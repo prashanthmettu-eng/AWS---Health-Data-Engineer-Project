5afbbd37b9b37125b6ed19f94c2365356925d020
prashanthmettu-eng <prashanthmettu@gmail.com>
2025-12-30 11:48:42 -0600

    Reverted: Pipeline triggers once in 3 months

--- infra/terraform/eventbridge.tf
+++ infra/terraform/eventbridge.tf
@@ -2,9 +2,9 @@ resource "aws_cloudwatch_event_rule" "health_pipeline_schedule" {
   name        = "${var.project_name}-pipeline-schedule"
   description = "Once in 3 months trigger for healthcare data pipeline"
 
-  schedule_expression = "cron(0/5 * * * ? *)" 
+  # schedule_expression = "cron(0/5 * * * ? *)" 
   # schedule_expression = "cron(0 1 * * ? *)"
-  # schedule_expression = "cron(0 1 1 */3 ? *)"
+  schedule_expression = "cron(0 1 1 */3 ? *)"
   
   tags = {
     Project     = var.project_name
